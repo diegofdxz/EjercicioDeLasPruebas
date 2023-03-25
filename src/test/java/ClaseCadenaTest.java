ce697fdba06bfe64a1cda350bd2d5f650e2130a0
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-03-25 22:33:51 +0100

    Codigo con tests

---
+++ src/test/java/ClaseCadenaTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClaseCadenaTest {
+
+    @org.junit.jupiter.api.Test
+    void devuelveIniciales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void interfaz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
